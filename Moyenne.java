17a6651826b166d25f051e9b36f85a35f83c1535
borgorre <julienmercierbeta@gmail.com>
2019-10-05 12:13:14 +0200

    je fais mon premier commit

---
+++ Moyenne.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Moyenne
+{
+	public static void main(String [] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int nbrEleve;
+		System.out.println("nombre d'élèves");
+		nbrEleve=sc.nextInt();
+		System.out.println(+nbrEleve);
+	}
+}
